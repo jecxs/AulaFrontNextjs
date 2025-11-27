58ff11161f6e4782c8358a35850e6e8e9162a5a2
Jeremy Castillo <josstinpretell3@gmail.com>
2025-11-27 15:11:27 -0500

    remove any rules - period

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
     images: {
         remotePatterns: [
             {
@@ -33,14 +39,7 @@ const nextConfig: NextConfig = {
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     },
-    eslint: {
-        // Ignora errores de ESLint durante el build
-        ignoreDuringBuilds: true,
-    },
-    typescript: {
-        // Ignora errores de TypeScript durante el build
-        ignoreBuildErrors: true,
-    },
+
 };
 
 export default nextConfig;
